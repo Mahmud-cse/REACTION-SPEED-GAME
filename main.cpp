d12ed11f215e34c445295108b27461b81f780053
S M Mahmud Hasan <52972839+Mahmud-cse@users.noreply.github.com>
2025-11-13 06:01:55 +0100

    Refactor button handling and buzzer functionality
    Updated button2 pin and modified buzzer delays. Added button press handling to control yellow LEDs.

--- main.cpp
+++ main.cpp
@@ -7,7 +7,9 @@ int redLED = 9;
 int buzzer = 7;
 
 int button1 = 13;
-int button2 = 6;
+int button2 = 5;
+
+bool buttonPressed;
 
 void setup() {
   pinMode(yellowLED1,OUTPUT);
@@ -15,27 +17,54 @@ void setup() {
   pinMode(greenLED,OUTPUT);
   pinMode(yellowLED,OUTPUT);
   pinMode(redLED,OUTPUT);
+
   pinMode(buzzer,OUTPUT);
+
   pinMode(button1,INPUT_PULLUP);
   pinMode(button2,INPUT_PULLUP);
+
+  randomSeed(analogRead(A0));
 }
 
 void loop() {
   digitalWrite(greenLED,HIGH);
-  delay(1000);
+  digitalWrite(buzzer,HIGH);
+  delay(100);
+  digitalWrite(buzzer,LOW);
+  delay(900);
   digitalWrite(greenLED,LOW);
 
   digitalWrite(yellowLED,HIGH);
-  delay(1000);
+  digitalWrite(buzzer,HIGH);
+  delay(100);
+  digitalWrite(buzzer,LOW);
+  delay(900);
   digitalWrite(yellowLED,LOW);
 
   digitalWrite(redLED,HIGH);
-  delay(1000);
+  digitalWrite(buzzer,HIGH);
+  delay(100);
+  digitalWrite(buzzer,LOW);
+  delay(random(900,4901));
   digitalWrite(redLED,LOW);
 
-  digitalWrite(yellowLED1,HIGH);
-  digitalWrite(yellowLED2,HIGH);
-  delay(1000);
+  while(buttonPressed == 0){
+    digitalWrite(yellowLED1,HIGH);
+    digitalWrite(yellowLED2,HIGH);
+
+    if(digitalRead(button1)==0){
+      buttonPressed = 1;
+      digitalWrite(yellowLED2,LOW);
+    }else if(digitalRead(button2)==0){
+      buttonPressed = 1;
+      digitalWrite(yellowLED1,LOW);
+    }
+  }
+  digitalWrite(buzzer,HIGH);
+  delay(500);
+  digitalWrite(buzzer,LOW);
+  delay(1500);
   digitalWrite(yellowLED1,LOW);
   digitalWrite(yellowLED2,LOW);
+  buttonPressed = 0;
 }
